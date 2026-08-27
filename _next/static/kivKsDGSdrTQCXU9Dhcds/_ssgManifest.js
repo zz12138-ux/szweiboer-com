56d8ea5a7e4bf04839c0b2661937a2585cf226c8
zz12138-ux <GabrielRogers1376@outlook.com>
2026-08-27 14:28:04 +0800

    feat: add dual-screen laptop product platforms

--- _next/static/kivKsDGSdrTQCXU9Dhcds/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Flaptops\u002F[slug]","\u002Fportable-monitors\u002F[slug]","\u002Fprojectors\u002F[slug]","\u002Ftablets\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
